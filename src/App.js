845d7154b7ca53d592f2152fa7e2ae177cc24679
Steven <steven.camillieri@protonmail.com>
2022-01-27 17:08:17 -0500

    layout top section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import React from 'react';
 import "./App.css";
 import Calc from "./components/Calc/Calc";
 import Display from "./components/Display/Display";
 
 function App() {
+  const [amount, setAmount] = React.useState("")
+  const [tip, setTip] = React.useState("")
   return (
     <div id="appWrap">
       <div id="splitterContainer">
         <p>SPLI</p>
         <p>TTER</p>
       </div>
-      <Calc />
-      <Display />
+      <div id="mainContainer">
+        <Calc />
+        <Display />
+      </div>
     </div>
   );
 }
--- src/components/Calc/Calc.js
+++ src/components/Calc/Calc.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import React from "react";
+import "./Calc.css";
 
 export default function Calc(props) {
-  return <div></div>;
+  const buttonArr = [1, 2, 3, 4, 5];
+
+  const buttonMap = buttonArr.map((i) => (
+    <input type="button" key={i} id={`number${i}`}></input>
+  ));
+  console.log(buttonMap);
+  return (
+    <div id="calcContainer">
+      <form id="billAmount">
+        <label htmlFor="amount">
+          Bill
+          <br />
+          <input
+            type="text"
+            id="amount"
+            placeholder="ie 142.55"
+            name="amount"
+          ></input>
+        </label>
+        <div id="buttonContainer">
+          <label>
+            Select Tip % <br />
+            <div id="buttonGrid">
+              {buttonMap}
+              <input type="text" placeholder="Custom" key="6" id="custom"></input>
+            </div>
+          </label>
+        </div>
+        <label htmlFor="people">
+          Number of People
+          <br />
+          <input
+            type="text"
+            id="people"
+            placeholder="ie 5"
+            name="people"
+          ></input>
+        </label>
+      </form>
+    </div>
+  );
 }
